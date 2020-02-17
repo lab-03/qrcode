ee7d4984c2bd1eb4615bff7335292e50027bbac5
IIZODIACII <amr_x14@outlook.com>
2020-02-17 23:29:09 +0200

    update heroku db config

--- database/config/config.js
+++ database/config/config.js
@@ -17,11 +17,10 @@ module.exports = {
   },
   production: {
     use_env_variable: process.env.DATABASE_URL,
-    // username: "ipauyltryqxtzw",
-    // password:
-    //   "b0e3d0ac9f8d6d1d6881bfd27f6c64ee304b3672facda708186d422f0081396e",
-    // database: process.env.DATABASE_URL,
-    // host: "ec2-54-247-125-38.eu-west-1.compute.amazonaws.com",
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
     dialect: "postgres",
     protocol: "postgres",
     dialectOption: {
