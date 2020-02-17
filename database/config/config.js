479fdd9b1e98e30ae29497adefd25d6823d9f64b
IIZODIACII <amr_x14@outlook.com>
2020-02-17 23:12:00 +0200

    update heroku db config

--- database/config/config.js
+++ database/config/config.js
@@ -16,11 +16,16 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "",
-    password: "",
-    database: "",
-    host: process.env.DATABASE_URL,
-    ssl: true,
-    dialect: "postgres"
+    use_env_variable: process.env.DATABASE_URL,
+    // username: "ipauyltryqxtzw",
+    // password:
+    //   "b0e3d0ac9f8d6d1d6881bfd27f6c64ee304b3672facda708186d422f0081396e",
+    // database: process.env.DATABASE_URL,
+    // host: "ec2-54-247-125-38.eu-west-1.compute.amazonaws.com",
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOption: {
+      ssl: true
+    }
   }
 };
--- database/models/index.js
+++ database/models/index.js
@@ -14,30 +14,25 @@ const db = {};
 
 let sequelize;
 if (config.environment === "production") {
-  // sequelize = new Sequelize(process.env.DATABASE_URL, config);
-  sequelize = new Sequelize(
-    // process.env.DATABASE_NAME,
-    // process.env.DATABASE_USER,
-    // process.env.DATABASE_PASS,
-    // {
-    //   host: process.env.DATABASE_URL,
-    //   port: process.env.DATABASE_PORT,
-    //   dialect: "postgres",
-    //   dialectOption: {
-    //     ssl: true,
-    //     native: true
-    //   },
-    //   logging: true
-    // }
-    config
-  );
+  sequelize = new Sequelize(config.database, config);
+  // sequelize = new Sequelize(
+  // process.env.DATABASE_NAME,
+  // process.env.DATABASE_USER,
+  // process.env.DATABASE_PASS,
+  // {
+  //   host: process.env.DATABASE_URL,
+  //   port: process.env.DATABASE_PORT,
+  //   dialect: "postgres",
+  //   dialectOption: {
+  //     ssl: true,
+  //     native: true
+  //   },
+  //   logging: true
+  // }
+  // config
+  // );
 } else {
-  sequelize = new Sequelize(
-    // config.database,
-    // config.username,
-    // config.password,
-    config
-  );
+  sequelize = new Sequelize(config);
 }
 
 fs.readdirSync(__dirname)
