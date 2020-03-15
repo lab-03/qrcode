52c71fe7c100cf1a39aaed7a1e327154e1079eb7
IIZODIACII <amr_x14@outlook.com>
2020-03-16 00:31:52 +0200

    heroku db connection issues fix

--- database/config/config.js
+++ database/config/config.js
@@ -2,29 +2,12 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "amr",
-    password: "root",
-    database: "qrcode_dev",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    use_env_variable: process.env.DB_DEV_URL
   },
   test: {
-    username: "amr",
-    password: "root",
-    database: "qrcode_test",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    use_env_variable: process.env.DB_TEST_URL
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
-    // username: process.env.DATABASE_USER,
-    // password: process.env.DATABASE_PASS,
-    // database: process.env.DATABASE_NAME,
-    // host: process.env.DATABASE_HOST,
-    dialect: "postgres",
-    protocol: "postgres",
-    dialectOption: {
-      ssl: true
-    }
+    use_env_variable: process.env.DATABASE_URL
   }
 };
--- database/models/index.js
+++ database/models/index.js
@@ -4,36 +4,23 @@ import Sequelize from "sequelize";
 import configJson from "../config/config";
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "production";
 
 const config = configJson[env];
 
 console.log("using environment: ", env);
+// console.log(config);
 
 const db = {};
 
-let sequelize;
-if (config.environment === "production") {
-  sequelize = new Sequelize(process.env.DATABASE_URL, config);
-  // sequelize = new Sequelize(
-  // process.env.DATABASE_NAME,
-  // process.env.DATABASE_USER,
-  // process.env.DATABASE_PASS,
-  // {
-  //   host: process.env.DATABASE_URL,
-  //   port: process.env.DATABASE_PORT,
-  //   dialect: "postgres",
-  //   dialectOption: {
-  //     ssl: true,
-  //     native: true
-  //   },
-  //   logging: true
-  // }
-  // config
-  // );
-} else {
-  sequelize = new Sequelize(config);
-}
+const options = {
+  dialect: "postgres",
+  ssl: true,
+  dialectOptions: { ssl: { require: true } },
+  logging: console.log
+};
+
+let sequelize = new Sequelize(config.use_env_variable, options);
 
 fs.readdirSync(__dirname)
   .filter(file => {
